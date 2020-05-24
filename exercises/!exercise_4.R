9ab185d64d340e9c93fe41db31fc445974be62f4
tkirschstein <thomas.kirschstein@wiwi.uni-halle.de>
2020-05-24 15:18:04 +0200

    rst

---
+++ exercises/!exercise_4.R
@@ -0,0 +1,18 @@
+---
+  title: "Exercise 4 - Newsvendor model"
+author: "Thomas Kirschstein"
+subtitle: Inventory Management
+output:
+  html_document:
+  df_print: paged
+toc: yes
+toc_float: true
+number_sections: false
+pdf_document: default
+---
+  
+  ```{r, setup, echo=F, include=T}
+library(DT)
+library(kableExtra)
+library(knitr)
+```
